a029259622ccb1fbab7bc9f0c3f5bace2175054e
chop <chopchoptop@outlook.com>
2024-04-13 20:45:38 +0800

    init

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import { testAdd } from '../index';
+
+test('My Greeter', () => {
+    expect(testAdd(1, 2)).toBe(3);
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const testAdd = (a: number, b: number) => a + b;
+
+export const testAdd2 = () => {
+    // new Promise(() => {});
+    const _aa = new Promise(() => {});
+    return '' + _aa;
+};
